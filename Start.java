d623b73db918b2a950f1170ab2b679c1b099eddd
Ram0806 <77051501+Ram0806@users.noreply.github.com>
2023-05-20 14:05:09 +0530

    Create Start.java
    First program

---
+++ Start.java
@@ -0,0 +1,33 @@
+class calculator {
+    public int sum(int a, int b) {
+        int result = a+b;
+        return result;
+    }
+}
+
+class compare {
+    public boolean check (int a, int b) {
+        boolean result = false;
+
+        if (a > b) {
+            result=true;
+            System.out.println("The value of " + a + " is greater than " + b);
+        }
+        return result;
+    }
+}
+
+public class Start {
+    public static void main(String[] args) {
+        int n1 = 70;
+        int n2 = 20;
+        System.out.println("Calling the calculator class");
+        calculator calc = new calculator();
+        int result = calc.sum(n1, n2);
+        System.out.println("Result is " + result);
+
+        compare comp = new compare();
+        boolean check =  comp.check(n1, n2);
+        System.out.println(check);
+    }
+}
