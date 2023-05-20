535b819d179e9218ca9a0f333a10399b4ddb48c0
Ram <s.ramakrishna.app@gmail.com>
2023-05-20 20:49:18 +0530

    comiiting for method overload

--- Start.java
+++ Start.java
@@ -1,8 +1,12 @@
 class calculator {
     public int sum(int a, int b) {
         int result = a+b;
-        System.out.print("Adding line back");
+        System.out.println("Adding line back");
         return result;
+    }
+
+    public int sum (int a,int b,int c) {
+        return a+b+c;
     }
 }
 
@@ -22,10 +26,13 @@ public class Start {
     public static void main(String[] args) {
         int n1 = 70;
         int n2 = 20;
+        int n3 = 50;
         System.out.println("Calling the calculator class");
         calculator calc = new calculator();
-        int result = calc.sum(n1, n2);
-        System.out.println("Result is " + result);
+        int r1 = calc.sum(n1, n2);
+        System.out.println("Result is " + r1);
+        int r2 = calc.sum(n1, n2, n3);
+        System.out.println("Result is " + r2);
 
         compare comp = new compare();
         boolean check =  comp.check(n1, n2);
